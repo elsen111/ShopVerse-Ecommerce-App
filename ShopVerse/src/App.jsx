0c85154938cc84756ff4c75f8b578bac80d6f0a7
elsen111 <csstudent1111@gmail.com>
2025-11-09 17:12:17 +0300

    Project created.

---
+++ ShopVerse/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-2xl text-blue bg-amber-200 p-6'>
+        ShopVerse
+      </h1>
+    </>
+  )
+}
+
+export default App
