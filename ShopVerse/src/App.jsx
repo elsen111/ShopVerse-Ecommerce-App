665af097daa5e1a69668ef97839e58a5484bed9b
elsen111 <csstudent1111@gmail.com>
2025-11-09 17:35:02 +0300

    Simple Routing tests

--- ShopVerse/src/App.jsx
+++ ShopVerse/src/App.jsx
@@ -1,13 +1,78 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/"> Home </Link>
+      <Link to="/about"> About </Link>
+      <Link to="/category"> Category </Link>
+      <Link to="/checkout"> Checkout </Link>
+      <Link to="/contact"> Contact </Link>
+    </nav>
+  );
+};
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <p> This is Home Page </p>
+    </>
+  );
+};
+
+const Category = () => {
+  return (
+    <>
+      <Navbar />
+      <p> This is Category Page </p>
+    </>
+  );
+};
+
+const Checkout = () => {
+  return (
+    <>
+      <Navbar />
+      <p> This is Checkout Page </p>
+    </>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <p> This is About Page </p>
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <p> This is Contact Page </p>
+    </>
+  );
+};
 
 function App() {
   return (
     <>
-      <h1 className='text-2xl text-blue bg-amber-200 p-6'>
-        ShopVerse
-      </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
