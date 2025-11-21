2b959b1160047b2bf10c3bcbebfdd6391e10e21b
elsen111 <csstudent1111@gmail.com>
2025-11-21 21:59:22 +0300

    ...

--- ShopVerse/src/components/_shared/Header/TopSection.jsx
+++ ShopVerse/src/components/_shared/Header/TopSection.jsx
@@ -1,14 +1,28 @@
 import { FaTruck } from "react-icons/fa6";
+import { IoIosArrowDown } from "react-icons/io";
 
 const TopSection = () => {
   return (
-    <section className="h-[50px] bg-amber-300">
-        <div>
-        <FaTruck />
-        <span className="text-primary-1"> Free shipping on orders over $50! </span>
-        </div>
-    </section>
-  )
-}
+    <section className="h-[50px] bg-amber-300 px-5 sm:px-8 flex justify-between flex-wrap items-center">
+      <div className="flex justify-start items-center gap-3">
+        <FaTruck className="text-amber-950 text-[30px]" />
+        <span className="text-amber-950 italic font-medium">
+          Free shipping on all orders over $50! Shop now and enjoy incredible
+          deals, limited-time offers, and exclusive discounts!
+        </span>
+      </div>
 
-export default TopSection
+      <div>
+        {/* Language Selection */}
+        {/* <div>
+            <div className="flex cursor-pointer items-center gap-1">
+                <span className="text-[16px]"> Eng </span>
+                <IoIosArrowDown className="text-[15px]" />
+            </div>
+        </div> */}
+      </div>
+    </section>
+  );
+};
+
+export default TopSection;
